9a57bce1076e314d0c98edf5dadda43f8a0d5e99
Shahbaz <shahbaz.hassan@7vals.com>
2023-08-31 12:46:43 +0500

    setup devise and mailcacher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,11 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Devise config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # mailcatcher config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
 end
