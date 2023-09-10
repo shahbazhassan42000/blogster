7f8269da18af5dfb1a759d2ec125aca6f67bb99d
Shahbaz <shahbaz.hassan@7vals.com>
2023-09-10 22:17:39 +0500

    blog crud completed

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   load_and_authorize_resource
+  include ApplicationHelper
+
   def create
     @blog = Blog.new(blog_params)
     @blog.company = current_user.company
@@ -13,6 +15,28 @@ class BlogsController < ApplicationController
       render 'blogs/new'
     end
   end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def destroy
+    @blog.destroy
+    if @blog.destroyed?
+      redirect_to dashboard_url(current_user), notice: 'Blog deleted successfully!'
+    else
+      redirect_to dashboard_url(current_user), alert: 'Error while deleting blog, please try again later.'
+    end
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to dashboard_url(current_user), notice: 'Blog updated successfully!'
+    else
+      redirect_to dashboard_url(current_user), alert: 'Error while updating blog, please try again later.'
+    end
+  end
+
   private
 
   def blog_params
