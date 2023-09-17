12f386054aee7d7e0600f0f32043fd1cb8d3ef91
Shahbaz <shahbaz.hassan@7vals.com>
2023-09-17 23:41:56 +0500

    added contributions notifications

--- app/helpers/contributors_helper.rb
+++ app/helpers/contributors_helper.rb
@@ -6,4 +6,8 @@ module ContributorsHelper
   def is_requested?
     @blog.contributors.exists?(current_user.id)
   end
+
+  def pending_requests_count
+    current_user.owner? ? BlogsUser.pending.count : BlogsUser.pending.where(blog_id: current_user.blogs.pluck(:id)).count
+  end
 end
